468a3cab2fae3b804fa235b32b651c3b11173daa
shailen16 <shailen16@gmail.com>
2021-08-08 07:55:06 +0100

    Revert "JIRA 1234 - Added unit tests 4-6"
    This reverts commit d4b52f19

--- src/test/java/Test_Application.java
+++ src/test/java/Test_Application.java
@@ -13,16 +13,4 @@ public class Test_Application {
     public void test3(){
         System.out.println("running test 3");
     }
-    @Test
-    public void test4(){
-        System.out.println("running test 4");
-    }
-    @Test
-    public void test5(){
-        System.out.println("running test 5");
-    }
-    @Test
-    public void test6(){
-        System.out.println("running test 6");
-    }
 }
